3e4b3c50792020911d224cc5fe4e27c8246e1ba6
amitshahworks <shahamitsuresh@gmail.com>
2026-06-04 17:35:56 +0530

    Day 2: Improved navbar styling and hover effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,60 @@
 function Navbar() {
   return (
     <nav
-    className="
-    sticky
-    top-0
-    bg-white
-    z-50
-    shadow-md
-    flex
-    justify-between
-    items-center
-    px-8
-    py-5
-"
->
-      <h1 className="text-2xl font-bold">Amit Shah</h1>
+      className="
+      sticky
+      top-0
+      bg-white/90
+      backdrop-blur-md
+      z-50
+      shadow-md
+      flex
+      justify-between
+      items-center
+      px-8
+      py-5
+      "
+    >
+      <h1 className="text-2xl font-bold">
+        Amit Shah
+      </h1>
 
       <ul className="hidden md:flex gap-8 font-medium">
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a
+            href="#about"
+            className="hover:text-blue-600 transition"
+          >
+            About
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#skills"
+            className="hover:text-blue-600 transition"
+          >
+            Skills
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#projects"
+            className="hover:text-blue-600 transition"
+          >
+            Projects
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#contact"
+            className="hover:text-blue-600 transition"
+          >
+            Contact
+          </a>
+        </li>
       </ul>
     </nav>
   );
